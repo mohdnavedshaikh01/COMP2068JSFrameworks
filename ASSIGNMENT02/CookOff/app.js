4327b34a052fa5a1aca06303ddd1b49206f01a5a
mohdnavedshaikh1908 <144464012+mohdnavedshaikh01@users.noreply.github.com>
2025-04-08 18:59:28 -0400

    Fix MongoClient reference

--- ASSIGNMENT02/CookOff/app.js
+++ ASSIGNMENT02/CookOff/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 const { engine } = require('express-handlebars');
+const { MongoClient } = require('mongodb'); 
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
